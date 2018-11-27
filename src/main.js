ff3f66a29cb23c5d84ce377a11cacb794a17cc8f
努力 <16768797@qq.com>
2018-11-27 09:29:03 +0800

    布局登录页面

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/reset.css'
 
+//引用插件
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
--- src/router/views/login.vue
+++ src/router/views/login.vue
@@ -45,4 +45,5 @@ export default {
   width: 100%;
 }
 
+
 </style>
