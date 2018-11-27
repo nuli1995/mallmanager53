9d57ae723564cfdf0ed8783929befe5f90c16079
努力 <16768797@qq.com>
2018-11-27 09:17:06 +0800

    布局登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/router/views/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   name: 'home',
+    //   path: '/',
+    //   redirect:{name:'login'}
+    // },
     {
+      name: 'login',
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Login
     }
   ]
 })
